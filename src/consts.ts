af60a1d227eb50085a844d1d7347a457ee539f1c
Theoleff <theodore.lef@gmail.com>
2024-10-01 17:28:32 +0200

    correct fonts & darkmode

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'TheoSoti';
-export const SITE_DESCRIPTION = "I'm a web developer teaching CSS tricks";
+export const SITE_DESCRIPTION = "I'm a frontend developer teaching CSS tips & tricks";
