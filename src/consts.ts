0cd4758959275fc2f048383a42819e6e3a99dc30
Theoleff <theodore.lef@gmail.com>
2025-03-06 22:32:18 +0100

    Correct some SEO issues

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'CSS Tips by Theo Soti | Web Design & Frontend Development';
 export const SITE_NAME = 'TheoSoti';
 export const SITE_DESCRIPTION =
-	'Learn CSS tips and tricks from freelance frontend developer Theo Soti. Explore in-depth guides, easy explanations, and creative design ideas to enhance your web projects.';
+	'Learn CSS tips & tricks with in-depth guides and creative design ideas. Enhance your web projects with expert insights from Theo Soti';
