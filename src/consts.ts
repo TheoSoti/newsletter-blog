393821ce3d22ff93e37f4de4129f30f7bc1e8e93
Theoleff <theodore.lef@gmail.com>
2025-03-07 20:24:24 +0100

    Small adjustments

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'CSS Tips by Theo Soti | Web Design & Frontend Development';
+export const SITE_TITLE = 'CSS Tips by Theo Soti | Web Design and Frontend Development';
 export const SITE_NAME = 'TheoSoti';
 export const SITE_DESCRIPTION =
 	'Learn CSS tips & tricks with in-depth guides and creative design ideas. Enhance your web projects with expert insights from Theo Soti';
