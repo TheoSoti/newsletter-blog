401f56f3db5ceda3a37642e7ee7888ff75a7dba4
Theoleff <theodore.lef@gmail.com>
2025-04-15 16:05:22 +0200

    change meta desc homepage

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'CSS Tips by Theo Soti | Web Design and Frontend Development';
 export const SITE_NAME = 'TheoSoti';
 export const SITE_DESCRIPTION =
-	'Learn CSS tips & tricks with in-depth guides and creative design ideas. Enhance your web projects with expert insights from Theo Soti';
+	'Theosoti is a frontend blog sharing CSS tutorials, practical guides, and bite-sized tips on modern web development and UI design.';
