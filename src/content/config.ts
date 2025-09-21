f1bad1205c4209820f9098211ca94a574606610a
Theoleff <theodore.lef@gmail.com>
2025-09-21 19:26:24 +0200

    Update microdata for shorts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,30 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		readingTime: z.string().optional(),
-	}),
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    readingTime: z.string().optional(),
+  }),
 });
 
 const short = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-	}),
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    heroImage: z.string().optional(),
+    updatedDate: z.coerce.date().optional(),
+  }),
 });
 
 export const collections = { blog, short };
